a0957718b96dc8f4b7d4da68a45a723fafcc4c01
Rodrigo <rodrigopereda24@gmail.com>
2024-06-23 03:00:19 -0500

    Conexion realizada

---
+++ src/main/java/com/example/demo/DemoLP2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLP2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLP2.class, args);
+	}
+
+}
